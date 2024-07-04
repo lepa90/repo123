f6488d8246c2f4d7edb506b58904f605838731bc
lepa90 <kamil.przylepa@gmail.com>
2024-07-04 13:13:50 +0200

    test

--- test_hi.py
+++ test_hi.py
@@ -5,12 +5,12 @@ class TestCalc(unittest.TestCase):
     """
     Test the add function from the calc library
     """
-    hi.hello()
+    
     def test_add_integers(self):
         """
         Test that the addition of two integers returns the correct total
         """
-
+        print("test")
         
 
 if __name__ == '__main__':
